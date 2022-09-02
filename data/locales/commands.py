5a6f41ccd5154eb84255726068d869896505b93f
Rag404 <105586263+Rag404@users.noreply.github.com>
2022-09-02 22:59:16 +0200

    fixed wrong local

--- data/locales/commands.py
+++ data/locales/commands.py
@@ -7,7 +7,7 @@ back_button = {
         "emoji": "<:back_arrow:940318470069960744>",
         "row": 4
     },
-    "fr-FR": {
+    "fr": {
         "label": "Retour",
         "emoji": "<:back_arrow:940318470069960744>",
         "row": 4
@@ -19,7 +19,7 @@ exit_button = {
         "label": "Exit",
         "style": ButtonStyle.red
     },
-    "fr-FR": {
+    "fr": {
         "label": "Sortir",
         "style": ButtonStyle.red
     }
@@ -31,7 +31,7 @@ missing_permission_embed = {
         description="You need to be an administrator to use this command!",
         color=Color.embed_background()
     ),
-    "fr-FR": Embed(
+    "fr": Embed(
         title="⛔ Permissions manquantes",
         description="Vous devez être administrateur pour utiliser cette commande !",
         color=Color.embed_background()
@@ -42,11 +42,11 @@ missing_permission_embed = {
 config_loc = {
     "name": {
         "en-US": "config",
-        "fr-FR": "config"
+        "fr": "config"
     },
     "description": {
         "en-US": "Admins only. Configure the bot for your server.",
-        "fr-FR": "Admins uniquement. Configurer le bot pour votre serveur."
+        "fr": "Admins uniquement. Configurer le bot pour votre serveur."
     },
     "embeds": {
         "main": {
@@ -55,7 +55,7 @@ config_loc = {
                 description="Select an option in the buttons bellow.",
                 color=Color.embed_background()
             ),
-            "fr-FR": Embed(
+            "fr": Embed(
                 title=":gear: Configurer Statys pour votre serveur",
                 description="Sélectionnez une option dans les buttons ci-dessous.",
                 color=Color.embed_background()
@@ -67,14 +67,14 @@ config_loc = {
                 description="**Select the role that will be given to the supporters.**",
                 color=Color.embed_background()
             ),
-            "fr-FR": Embed(
+            "fr": Embed(
                 title="🎭 Sélectionner un rôle",
                 description="**Sélectionnez le rôle qui sera donné aux supporteurs.**",
                 color=Color.embed_background()
             ),
             "warning": {
                 "en-US": "\n\n⚠ Your server seems to have more roles than the menu can handle. Use `/set-supporter` instead if you don't see the role you want in the dropdown.",
-                "fr-FR": "\n\n⚠ Votre serveur à l'air d'avoir plus de rôles que le menu ne peut supporter. Utiliser `/set-supporteur` si vous ne trouvez pas le rôle que vous voulez dans la sélection."
+                "fr": "\n\n⚠ Votre serveur à l'air d'avoir plus de rôles que le menu ne peut supporter. Utiliser `/set-supporteur` si vous ne trouvez pas le rôle que vous voulez dans la sélection."
             }
         },
         "exclude_roles": {
@@ -82,30 +82,30 @@ config_loc = {
                 title="❌ Exclude roles",
                 description="**The satus of the members with the selected roles won't be checked.**\n\n*Note: If one of the selected roles is also the role given to supporters, the members will never lose this role after getting it.*"
             ),
-            "fr-FR": Embed(
+            "fr": Embed(
                 title="❌ Exclure des rôles",
                 description="**Le statut des membres avec les rôles sélectionnés ne sera pas vérifié.**\n\n*Note : Si un des rôles sélectionnés est aussi le rôle supporteur, les membres ne perdront jamais ce rôle après l'avoir obtenu.*"
             ),
             "warning": {
                 "en-US": "\n\n⚠ Your server seems to have more roles than the menu can handle. Use `/add-excluded` and `/remove-excluded` instead if you don't see the role you want in the dropdown.",
-                "fr-FR": "\n\n⚠ Votre serveur à l'air d'avoir plus de rôles que le menu ne peut supporter. Utiliser `/ajouter-exception` et `/enlever-exception` si vous ne trouvez pas le rôle que vous voulez dans la sélection."
+                "fr": "\n\n⚠ Votre serveur à l'air d'avoir plus de rôles que le menu ne peut supporter. Utiliser `/ajouter-exception` et `/enlever-exception` si vous ne trouvez pas le rôle que vous voulez dans la sélection."
             }
         }
     },
     "buttons": {
         "select_supporter": {
             "en-US": "Role",
-            "fr-FR": "Rôle"
+            "fr": "Rôle"
         },
         "exclude_roles": {
             "en-US": "Exclude",
-            "fr-FR": "Exclure"
+            "fr": "Exclure"
         }
     },
     "dropdowns": {
         "select_supporter": {
             "en-US": "Do not give a role",
-            "fr-FR": "Ne pas donner de rôle"
+            "fr": "Ne pas donner de rôle"
         }
     }
 }
@@ -113,10 +113,10 @@ config_loc = {
 set_supporter_loc = {
     "name": {
         "en-US": "set-supporter",
-        "fr-FR": "set-supporteur"
+        "fr": "set-supporteur"
     },
     "description": {
         "en-US": "Admins only. Set the supporter role for this server.",
-        "fr-FR": "Admins uniquement. Définir le rôle supporter pour votre serveur."
+        "fr": "Admins uniquement. Définir le rôle supporter pour votre serveur."
     }
 }
